96d7a87d9cf38075120d6ddfe30d99611d30f119
jtech mind <jtechmind@gmail.com>
2024-01-02 22:10:37 +0530

    DataTypes class added with simply main method

---
+++ src/main/java/com/core/datatypes/DataTypes.java
@@ -0,0 +1,10 @@
+package com.core.datatypes;
+
+public class DataTypes {
+    /**
+     *
+     */
+    public static void main(String[] args) {
+        System.out.println("Running from DataTypes.");
+    }
+}
