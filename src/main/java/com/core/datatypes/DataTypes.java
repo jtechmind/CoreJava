c951bcd37c1db86f81dc52a025c597f6739d4502
jtech mind <jtechmind@gmail.com>
2024-01-06 19:59:39 +0530

    primitive types

--- src/main/java/com/core/datatypes/DataTypes.java
+++ src/main/java/com/core/datatypes/DataTypes.java
@@ -2,9 +2,33 @@ package com.core.datatypes;
 
 public class DataTypes {
     /**
-     *
+     *Primitive Data Types:
+     * int
+     * byte
+     * short
+     * long
+     * float
+     * double
+     * boolean
+     * char
+     * Non-Primitive Data Types
+     * Array
+     * Class
+     * Interface
+     * String
+     * Enum
      */
     public static void main(String[] args) {
         System.out.println("Running from DataTypes.");
+        byte age = 22;
+        short year = 2024;
+        int score = 99;
+        float percent = 94.45f;
+        double db = 125.125d;
+        char grade = 'A';
+
+
+
+
     }
 }
